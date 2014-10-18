f3aebb051175c847216e2c81b6ac0df57a8d13c2
Stefan <stf.andersson@gmail.com>
2014-10-18 02:14:44 +0200

    Added basic StarManager.cs
    Added a basic StarManager.cs and made it compatible with Star.cs. Maybe
    not the prettiest code though >.>

--- Assets/Scripts/Star.cs
+++ Assets/Scripts/Star.cs
@@ -3,117 +3,113 @@ using System.Collections.Generic;
 
 public class Star : MonoBehaviour {
 
-    public enum Type { cl1, cl2, cl3, cl4, cl5, cl6, cl7, cl8, cl9, cl10 }
+    public enum Type { cl0, cl1, cl2, cl3, cl4, cl5, cl6, cl7, cl8, cl9 }
     public Type type;
 
     public List<Star> connections;
     public float age;
-    //public float energy
-    //{
-    //    //get 
-    //    //{
-    //    //    switch (type)
-    //    //    {
-    //    //        case Type.cl1:
-    //    //            return StarManager.energy_cl1;
+    public float energy
+    {
+        get
+        {
+            switch (type)
+            {
+                case Type.cl0:
+                    return StarManager.Instance.starClassEnergies[0];
 
-    //    //        case Type.cl2:
-    //    //            return StarManager.energy_cl2;
+                case Type.cl1:
+                    return StarManager.Instance.starClassEnergies[1];
 
-    //    //        case Type.cl3:
-    //    //            return StarManager.energy_cl3;
+                case Type.cl2:
+                    return StarManager.Instance.starClassEnergies[2];
 
-    //    //        case Type.cl4:
-    //    //            return StarManager.energy_cl4;
+                case Type.cl3:
+                    return StarManager.Instance.starClassEnergies[3];
 
-    //    //        case Type.cl5:
-    //    //            return StarManager.energy_cl5;
+                case Type.cl4:
+                    return StarManager.Instance.starClassEnergies[4];
 
-    //    //        case Type.cl6:
-    //    //            return StarManager.energy_cl6;
+                case Type.cl5:
+                    return StarManager.Instance.starClassEnergies[5];
 
-    //    //        case Type.cl7:
-    //    //            return StarManager.energy_cl7;
+                case Type.cl6:
+                    return StarManager.Instance.starClassEnergies[6];
 
-    //    //        case Type.cl8:
-    //    //            return StarManager.energy_cl8;
+                case Type.cl7:
+                    return StarManager.Instance.starClassEnergies[7];
 
-    //    //        case Type.cl9:
-    //    //            return StarManager.energy_cl9;
+                case Type.cl8:
+                    return StarManager.Instance.starClassEnergies[8];
 
-    //    //        case Type.cl10:
-    //    //            return StarManager.energy_cl10;
-    //    //    }
-    //    //}
-    //}
-    //public float gravity
-    //{ 
-    //    //get 
-    //    //{
-    //    //    switch (type)
-    //    //    {
-    //    //        case Type.cl1:
-    //    //            return StarManager.gravity_cl1;
+                case Type.cl9:
+                    return StarManager.Instance.starClassEnergies[9];
 
-    //    //        case Type.cl2:
-    //    //            return StarManager.gravity_cl2;
+                default:
+                    return 0;
+            }
+        }
+    }
+    public float gravity
+    {
+        get
+        {
+            switch (type)
+            {
+                case Type.cl0:
+                    return StarManager.Instance.starClassGravities[0];
 
-    //    //        case Type.cl3:
-    //    //            return StarManager.gravity_cl3;
+                case Type.cl1:
+                    return StarManager.Instance.starClassGravities[1];
 
-    //    //        case Type.cl4:
-    //    //            return StarManager.gravity_cl4;
+                case Type.cl2:
+                    return StarManager.Instance.starClassGravities[2];
 
-    //    //        case Type.cl5:
-    //    //            return StarManager.gravity_cl5;
+                case Type.cl3:
+                    return StarManager.Instance.starClassGravities[3];
 
-    //    //        case Type.cl6:
-    //    //            return StarManager.gravity_cl6;
+                case Type.cl4:
+                    return StarManager.Instance.starClassGravities[4];
 
-    //    //        case Type.cl7:
-    //    //            return StarManager.gravity_cl7;
+                case Type.cl5:
+                    return StarManager.Instance.starClassGravities[5];
 
-    //    //        case Type.cl8:
-    //    //            return StarManager.gravity_cl8;
+                case Type.cl6:
+                    return StarManager.Instance.starClassGravities[6];
 
-    //    //        case Type.cl9:
-    //    //            return StarManager.gravity_cl9;
+                case Type.cl7:
+                    return StarManager.Instance.starClassGravities[7];
 
-    //    //        case Type.cl10:
-    //    //            return StarManager.gravity_cl10;
-    //    //    }
-    //    //}
-    //}
+                case Type.cl8:
+                    return StarManager.Instance.starClassGravities[8];
+
+                case Type.cl9:
+                    return StarManager.Instance.starClassGravities[9];
+
+                default:
+                    return 0;
+            }
+        }
+    }
 
     void Awake()
     {
         Setup();
     }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
     void Setup()
     {
-        //int _rand = Random.Range(0, 101);
+        int _rand = Random.Range(0, 101);
 
-        //     if (_rand <= StarManager.probability_cl1) type = Type.cl1;
-        //else if (_rand <= StarManager.probability_cl2) type = Type.cl2;
-        //else if (_rand <= StarManager.probability_cl3) type = Type.cl3;
-        //else if (_rand <= StarManager.probability_cl4) type = Type.cl4;
-        //else if (_rand <= StarManager.probability_cl5) type = Type.cl5;
-        //else if (_rand <= StarManager.probability_cl6) type = Type.cl6;
-        //else if (_rand <= StarManager.probability_cl7) type = Type.cl7;
-        //else if (_rand <= StarManager.probability_cl8) type = Type.cl8;
-        //else if (_rand <= StarManager.probability_cl9) type = Type.cl9;
-        //else if (_rand <= StarManager.probability_cl10) type = Type.cl10;
+             if (_rand <= StarManager.Instance.starClassProbabilities[0]) type = Type.cl0;
+        else if (_rand <= StarManager.Instance.starClassProbabilities[1]) type = Type.cl1;
+        else if (_rand <= StarManager.Instance.starClassProbabilities[2]) type = Type.cl2;
+        else if (_rand <= StarManager.Instance.starClassProbabilities[3]) type = Type.cl3;
+        else if (_rand <= StarManager.Instance.starClassProbabilities[4]) type = Type.cl4;
+        else if (_rand <= StarManager.Instance.starClassProbabilities[5]) type = Type.cl5;
+        else if (_rand <= StarManager.Instance.starClassProbabilities[6]) type = Type.cl6;
+        else if (_rand <= StarManager.Instance.starClassProbabilities[7]) type = Type.cl7;
+        else if (_rand <= StarManager.Instance.starClassProbabilities[8]) type = Type.cl8;
+        else if (_rand <= StarManager.Instance.starClassProbabilities[9]) type = Type.cl9;
 
 
         age = Random.Range(1, 13);
---
+++ Assets/Scripts/StarManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StarManager : MonoBehaviour {
+
+    public static StarManager Instance;
+
+    [HideInInspector] public List<Star> allStars;
+
+    public List<float> starClassEnergies;
+    public List<float> starClassGravities;
+    public List<float> starClassProbabilities;
+
+    void Awake()
+    {
+        if (Instance) Destroy(this);
+        else Instance = this;
+    }
+
+	void Start () 
+    {
+        StartCoroutine(UpdateStarAges(1f));
+	}
+
+
+    IEnumerator UpdateStarAges(float _updateFreq)
+    {
+        for (; ;)
+        {
+            foreach (Star _star in allStars)
+            {
+                //Update age for each star
+            }
+
+            yield return new WaitForSeconds(_updateFreq);
+        }
+    }
+}
