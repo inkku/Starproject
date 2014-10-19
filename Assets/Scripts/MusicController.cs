980a6e25df6a94fb917c19759e3c3337795120d4
j0pr <johannprell@johanns-mbp.kansliet>
2014-10-19 13:15:35 +0200

    Adaptive mix 01
    Hardcoded mixes for different kinds of stars, changes music mix
    according to energy level of current star

--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -20,6 +20,14 @@ public class MusicController : MonoBehaviour {
 	public float[] stemVol = new float[6]; 
 	public bool[] onBeat = new bool[6];
 
+	public float[] mix1 = new float[] {0f, 0f, 1f, 0f, 0f, 0f};
+	public float[] mix2 = new float[] {0f, 0.6f, 1f, 0.2f, 0f, 0.3f};
+	public float[] mix3 = new float[] {0f, 1f, 0.8f, 0.6f, 0.4f, 0.75f};
+	public float[] mix4 = new float[] {0.6f, 1f, 0.6f, 0.8f, 0.8f, 1f};
+	public float[] mix5 = new float[] {0.8f, 1f, 0.3f, 1f, 1f, 1f};
+	public float[] mix6 = new float[] {1f, 1f, 0.2f, 1f, 1f, 1f};
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -53,15 +61,67 @@ public class MusicController : MonoBehaviour {
 			s.dopplerLevel = 0;
 				}
 
-		stemVol [2] = 1;
+		//stemVol [2] = 1;
+		//float setPulseVol = 0.3f;
+		//stemVol [1] = 0.5f;
+
+		mix1 = new float[] {0f, 0f, 1f, 0f, 0f, 0f};
+		mix2 = new float[] {0f, 0.6f, 1f, 0.2f, 0f, 0.3f};
+		mix3 = new float[] {0f, 1f, 0.8f, 0.6f, 0.4f, 0.75f};
+		mix4 = new float[] {0.6f, 1f, 0.6f, 0.8f, 0.8f, 1f};
+		mix5 = new float[] {0.8f, 1f, 0.3f, 1f, 1f, 1f};
+		mix6 = new float[] {1f, 1f, 0.2f, 1f, 1f, 1f};
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-				beatTimer += Time.deltaTime;
+		beatTimer += Time.deltaTime;
+		int currentStar = GameHandler.Instance.currentStar.typeNum;
 
+		switch (currentStar)
+		{
+		case 0:
+			for (int i = 0; i < stemVol.Length; i++) {
+				stemVol[i] = mix1[i];
+				Debug.Log("mix1");
+			}
+			break;
+		case 1:
+			for (int i = 0; i < stemVol.Length; i++) {
+				stemVol[i] = mix2[i];
+				Debug.Log("mix1");
+			}
+			break;
+		case 2:
+			for (int i = 0; i < stemVol.Length; i++) {
+				stemVol[i] = mix2[i];
+			}
+			break;
+		case 3:
+			for (int i = 0; i < stemVol.Length; i++) {
+				stemVol[i] = mix3[i];
+			}
+			break;
+		case 4:
+			for (int i = 0; i < stemVol.Length; i++) {
+				stemVol[i] = mix4[i];
+			}
+			break;
+		case 5:
+			for (int i = 0; i < stemVol.Length; i++) {
+				stemVol[i] = mix5[i];
+			}
+			break;
+		case 6:
+			for (int i = 0; i < stemVol.Length; i++) {
+				stemVol[i] = mix6[i];
+			}
+			break;
 
+			
+
+		}
 
 
 		for (int i=0; i<6; i++) {
